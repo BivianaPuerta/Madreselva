3449d8cde62d412f2db55d14d6d1c06905b2af88
Biviana Puerta <92127794+BivianaPuerta@users.noreply.github.com>
2021-12-14 22:11:21 -0500

    Add files via upload
    Update Reto3-Reto4

---
+++ reto3-reto4/src/main/java/com/madreselva/reto3/service/UserService.java
@@ -0,0 +1,152 @@
+package com.madreselva.reto3.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.madreselva.reto3.model.User;
+import com.madreselva.reto3.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Biviana Puerta
+ */
+@Service
+public class UserService {
+    /**
+     * Autowired
+     */
+    @Autowired
+    private UserRepository userRepository;
+
+    /**
+     * 
+     * List User
+     * @return List
+     */
+    public List<User> getAll(){
+        return userRepository.getAll();
+    }
+    /**
+     * Optional User
+     * @param id
+     * @return integer 
+     */
+    public Optional<User> getUsers(int id){
+        return userRepository.getUser(id);
+    }
+    /**
+     * boolean emailexist
+     * @return boolean
+     */
+    public boolean emailexist(String email){
+        return userRepository.emailexist(email);
+    }
+
+    /**
+     * userAuthentication
+     * @param email
+     * @param password
+     * @return String
+     */
+    public User userAuthentication(String email, String password){
+        Optional <User> users= userRepository.userAuthentication(email, password);
+
+        if(users.isEmpty()){
+            return new User();
+        }
+        else{
+            return users.get();
+        }
+    }
+
+
+    /**
+     * User create
+     * @param user
+     * @return user
+     */
+    public User create(User user){
+
+        Optional<User> userIdMaximo = userRepository.lastUserId();
+        
+            if (user.getId() == null) {
+                if (userIdMaximo.isEmpty())
+                    user.setId(1);
+                else
+                    user.setId(userIdMaximo.get().getId() + 1);
+        }
+            Optional <User> exist = userRepository.getUser(user.getId());
+            if (exist.isEmpty()){
+                if(emailexist(user.getEmail())==false){
+                    return userRepository.create(user);
+                }
+                else{
+                    return user;
+                }
+            }
+            else{
+                return user;
+            }
+        }
+
+        /**
+         * User update
+         * @param user
+         * @return atributos user
+         */
+    public User update (User user){
+        if (user.getId() !=null){
+            Optional <User> userDateBase = userRepository.getUser(user.getId());
+            if(!userDateBase.isEmpty()){
+                if(user.getIdentification() !=null){
+                    userDateBase.get().setIdentification(user.getIdentification());
+                }
+                if (user.getName()!=null){
+                    userDateBase.get().setName(user.getName());
+                }
+                if (user.getMonthBirthtDay()!=null){
+                    userDateBase.get().setMonthBirthtDay(user.getMonthBirthtDay());
+                }
+                if (user.getAddress()!=null){
+                    userDateBase.get().setAddress(user.getAddress());
+                }
+                if (user.getCellPhone()!=null){
+                    userDateBase.get().setCellPhone(user.getCellPhone());
+                }
+                if (user.getEmail()!=null){
+                    userDateBase.get().setEmail(user.getEmail());
+                }
+                if (user.getPassword()!=null){
+                    userDateBase.get().setPassword(user.getPassword());
+                }
+                if (user.getZone()!=null){
+                    userDateBase.get().setZone(user.getZone());
+                }
+                if (user.getType()!=null){
+                    userDateBase.get().setType(user.getType());
+                }
+                userRepository.update(userDateBase.get());
+                    return userDateBase.get();
+            }else{
+                return user;
+            }
+        }return user;
+    }
+    /**
+     * boolean deleteUser
+     * @param id
+     * @return boolean
+     */
+
+    public boolean deleteUser(int id){
+        Optional <User> exist= userRepository.getUser(id);
+        if(!exist.isEmpty()){
+            userRepository.delete(exist.get());
+
+            return true;
+        }
+        return false;
+    }
+}
---
+++ reto3-reto4/src/main/java/com/madreselva/reto3/web/UserController.java
@@ -0,0 +1,107 @@
+package com.madreselva.reto3.web;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.madreselva.reto3.model.User;
+import com.madreselva.reto3.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Biviana Puerta
+ */
+@RestController
+@RequestMapping("/api/user/")
+@CrossOrigin("*")
+
+/**
+ * class UserController
+ */
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    /**
+     * Get Mapping
+     * @return userService
+     */
+    @GetMapping("/all")
+    public List<User> getAll(){
+        return userService.getAll();
+    }
+
+    /**
+     * Get Mapping
+     * @return userService
+     */
+    @GetMapping("/{id}")
+    public Optional <User> getUser(@PathVariable("id") int id) {
+        return userService.getUsers(id);
+    }
+
+    /**
+     * GetMapping
+     * @param email
+     * @return boolean 
+     */
+    @GetMapping("/emailexist/{email}")
+    public boolean emailexist(@PathVariable("email") String email){
+        return userService.emailexist(email);
+    }
+
+    /**
+     * GetMapping
+     * @param email
+     * @param password
+     * @return userAuthentication
+     */
+    @GetMapping("/{email}/{password}")
+    public User userAuthentcation(@PathVariable("email")String email, @PathVariable("password")String password){
+        return userService.userAuthentication(email, password);
+    }
+
+    /**
+     * PostMapping
+     * @param user
+     * @return userService
+     */
+    @PostMapping("/new")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User create(@RequestBody User user){
+        return userService.create(user);
+    }
+
+    /**
+     * PutMapping
+     * @param user
+     * @return userService
+     */
+    @PutMapping("/update")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User update(@RequestBody User user){
+        return userService.update(user);
+    }
+
+    /**
+     * DeleteMapping
+     * @param id
+     * @return userService
+     */
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public boolean deleteUser(@PathVariable("id")int id){
+        return userService.deleteUser(id);
+    }
+
+}
+
